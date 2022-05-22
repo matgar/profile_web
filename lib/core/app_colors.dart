373b04e4a5eead6505a495cb7ba6941c91713e6b
Mateus Garcia <matgar@gmail.com>
2022-05-22 10:51:52 -0300

    Updating dark theme colors

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -4,11 +4,7 @@ class AppColors {
   const AppColors._();
 
   static const primaryColor = Color(0xffc8e6c9);
-  static const primaryLightColor = Color(0xfffbfffc);
-  static const primaryDarkColor = Color(0xff97b498);
   static const secondaryColor = Color(0xff212121);
-  static const secondaryLightColor = Color(0xff484848);
-  static const secondaryDarkColor = Color(0xff000000);
   static const onPrimaryColor = Color(0xff000000);
   static const onSecondaryColor = Color(0xffffffff);
   static const errorColor = Color(0xffb3261e);
@@ -19,13 +15,9 @@ class AppColors {
   static const onSurfaceColor = Color(0xff191c1a);
 
   static const darkThemePrimaryColor = Color(0xff424242);
-  static const darkThemePrimaryLightColor = Color(0xff6d6d6d);
-  static const darkThemePrimaryDarkColor = Color(0xff1b1b1b);
-  static const darkThemeSecondaryColor = Color(0xff69f0ae);
-  static const darkThemeSecondaryLightColor = Color(0xff9fffe0);
-  static const darkThemeSecondaryDarkColor = Color(0xff2bbd7e);
+  static const darkThemeSecondaryColor = Color(0xff92d786);
   static const darkThemeOnPrimaryColor = Color(0xffffffff);
-  static const darkThemeOnSecondaryColor = Color(0xff000000);
+  static const darkThemeOnSecondaryColor = Color(0xff013905);
   static const darkThemeErrorColor = Color(0xfff2b8b5);
   static const darkThemeOnErrorColor = Color(0xff601410);
   static const darkThemeBackgroundColor = Color(0xff191c1a);
